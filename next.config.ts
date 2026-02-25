2a0735695e1a5bf9b1849ac30684a8f41b6b7bbd
Himanip04 <103876032+Himanip04@users.noreply.github.com>
2026-02-25 12:22:30 +0530

    Add basePath and assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   output: "export",
+  output: "export",
+  basePath: "/job-portal-repo",
+  assetPrefix: "/job-portal-repo/",
 };
 
 export default nextConfig;
